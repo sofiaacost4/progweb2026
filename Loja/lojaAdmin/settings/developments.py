f9369bb6706d5f46fdf1f7ae86a72dcd60dd5e25
sofiaacost4 <k.sofia@escolar.ifrn.edu.br>
2026-03-27 10:07:53 -0300

    segunda atualização

---
+++ Loja/lojaAdmin/settings/developments.py
@@ -0,0 +1,10 @@
+from .settings import*
+DEBUG = True
+#Crie secret key para seu ambiente de desenvolvimento
+SECRET_KEY='ixb62ha#ts=ab4t2u%p1_62-!5w2j==j6d^3-j$!z(@*m+-h'
+DATABASES={
+    'default':{
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}

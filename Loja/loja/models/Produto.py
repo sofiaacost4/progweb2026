88cec1b85fff66ab89d2219ca0f2cd7e780cba8c
sofiaacost4 <k.sofia@escolar.ifrn.edu.br>
2026-04-13 10:20:19 -0300

    cap 3 pt 3 final

--- Loja/loja/models/Produto.py
+++ Loja/loja/models/Produto.py
@@ -10,5 +10,6 @@ class Produto(models.Model):
     fabricante = models.ForeignKey(Fabricante, null=True, related_name='fabricante', on_delete=models.SET_NULL)
     criado_em = models.DateTimeField(auto_now_add=True)
     alterado_em = models.DateTimeField(auto_now=True)
+    image = models.ImageField(null=True, blank=True)
     def __str__(self):
         return '{}'.format(self.Produto)

4110a1ac4dcf1efe72881b590a9d5c2c4ffd6134
sofia <k.sofia@escolar.ifrn.edu.br>
2026-05-11 10:30:29 -0300

    cap 6 pt 1

--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -1,5 +1,33 @@
 from django.http import HttpResponse
+from loja.models import Produto
 def list_produto_view(request, id=None):
-    if id is None:
-        return HttpResponse('<h1>Nenhum id foi informado</h1>')
+    produto = request.GET.get("produto")
+    destaque = request.GET.get("destaque")
+    promocao = request.GET.get("promocao")
+    categoria = request.GET.get("categoria")
+    fabricante = request.GET.get("fabricante")
+    produtos = Produto.objects.all()
+    if produto is not None:
+        produtos = produtos.filter(Produto=produto)
+    if promocao is not None:
+        produtos = produtos.filter(promocao=promocao)
+    if destaque is not None:
+        produtos = produtos.filter(destaque=destaque)
+    if categoria is not None:
+        produtos = produtos.filter(categoria=categoria)
+    if fabricante is not None:
+        produtos = produtos.filter(fabricante=fabricante)
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    print(produtos)
     return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+
+
+#cd loja
+#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+#winget install Python.Python.3.11
+#py -3.11 -m venv venv
+#venv\Scripts\activate
+#pip install django Django==4.2.7
+#pip install pillow
+#python manage.py runserver 127.0.0.1:8080

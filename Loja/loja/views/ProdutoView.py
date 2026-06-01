d87e62a15047232a95d411e51f8d1a616fb55b66
sofia <k.sofia@escolar.ifrn.edu.br>
2026-06-01 10:05:31 -0300

    cap 7 pt 4 botão salvar e botão ver

--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -1,8 +1,7 @@
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
-from django.shortcuts import render
-from loja.models import Produto
+from django.shortcuts import render, redirect
 
 def edit_produto_view(request, id=None):
     produtos = Produto.objects.all()
@@ -42,6 +41,40 @@ def list_produto_view(request, id=None):
     context = {'produtos': produtos}
     return render(request, template_name='produto/produto.html', context=context, status=200)
 
+def edit_produto_postback(request, id=None):
+    if request.method == 'POST':
+        id = request.POST.get("id")
+        produto = request.POST.get("Produto")
+        destaque = request.POST.get("destaque")
+        promocao = request.POST.get("promocao")
+        msgPromocao = request.POST.get("msgPromocao")
+        print("postback")
+        print(id)
+        print(produto)
+        print(destaque)
+        print(promocao)
+        print(msgPromocao)
+        try:
+            obj_produto = Produto.objects.filter(id=id).first()
+            obj_produto.Produto = produto
+            obj_produto.destaque = (destaque is not None)
+            obj_produto.promocao = (promocao is not None)
+            if msgPromocao is not None:
+                obj_produto.msgPromocao = msgPromocao
+            obj_produto.save()
+            print("Produto %s salvo com sucesso" % produto)
+        except Exception as e:
+            print("Erro salvando edição de produto: %s" % e)
+    return redirect("/produto")
+
+def details_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = {'produto': produto}
+    return render(request, template_name='produto/produto-details.html', context=context,status=200)
 
 #cd loja
 #Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser

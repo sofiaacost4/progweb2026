3909d2f4c089f1eb945702f6832c1a57415fcb67
sofiaacost4 <k.sofia@escolar.ifrn.edu.br>
2026-06-12 09:59:45 -0300

    cap 7 inserir produto pt final

--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -1,15 +1,18 @@
-from loja.models import Produto
+from loja.models import Produto, Fabricante, Categoria
 from datetime import timedelta, datetime
 from django.utils import timezone
 from django.shortcuts import render, redirect
+from django.core.files.storage import FileSystemStorage
 
 def edit_produto_view(request, id=None):
     produtos = Produto.objects.all()
+    Fabricantes = Fabricante.objects.all()
+    Categorias = Categoria.objects.all()
     if id is not None:
         produtos = produtos.filter(id=id)
     produto = produtos.first()
     print(produto)
-    context = { 'produto': produto }
+    context = { 'produto': produto, 'fabricantes' : Fabricantes, 'categorias' : Categorias}
     return render(request, template_name='produto/produto-edit.html', context=context, status=200)
 
 def edit_produto_postback(request, id=None):
@@ -19,6 +22,8 @@ def edit_produto_postback(request, id=None):
         destaque = request.POST.get("destaque")
         promocao = request.POST.get("promocao")
         msgPromocao = request.POST.get("msgPromocao")
+        categoria = request.POST.get("CategoriaFk")
+        fabricante = request.POST.get("FabricanteFk")
         print("postback")
         print(id)
         print(produto)
@@ -30,6 +35,8 @@ def edit_produto_postback(request, id=None):
             obj_produto.Produto = produto
             obj_produto.destaque = (destaque is not None)
             obj_produto.promocao = (promocao is not None)
+            obj_produto.fabricante = Fabricante.objects.filter(id=fabricante).first()
+            obj_produto.categoria = Categoria.objects.filter(id=categoria).first()
             if msgPromocao is not None:
                 obj_produto.msgPromocao = msgPromocao
             obj_produto.save()
@@ -99,6 +106,47 @@ def delete_produto_postback(request, id=None):
         return redirect("/produto")
 
 def create_produto_view(request, id=None):
+    if request.method == 'POST':
+        produto = request.POST.get("Produto")
+        destaque = request.POST.get("destaque")
+        promocao = request.POST.get("promocao")
+        msgPromocao = request.POST.get("msgPromocao")
+        preco = request.POST.get("preco")
+        image = request.POST.get("image")
+        print("postback-create")
+        print(produto)
+        print(destaque)
+        print(promocao)
+        print(msgPromocao)
+        print(preco)
+        print(image)
+        try:
+            obj_produto = Produto()
+            obj_produto.Produto = produto
+            obj_produto.destaque = (destaque is not None)
+            obj_produto.promocao = (promocao is not None)
+            if msgPromocao is not None:
+                obj_produto.msgPromocao = msgPromocao
+            obj_produto.preco = 0
+            if (preco is not None) and ( preco != ""):
+                obj_produto.preco = preco
+            obj_produto.criado_em = timezone.now()
+            obj_produto.alterado_em = obj_produto.criado_em
+            # Se for anexado arquivo, salva na pasta e guarda nome no objeto
+            if request.FILES is not None:
+                num_files = len(request.FILES.getlist('image'))
+                if num_files > 0:
+                    imagefile = request.FILES['image']
+                    print(imagefile)
+                    fs = FileSystemStorage()
+                    filename = fs.save(imagefile.name, imagefile)
+                    if (filename is not None) and (filename != ""):
+                        obj_produto.image = filename
+            obj_produto.save()
+            print("Produto %s salvo com sucesso" % produto)
+        except Exception as e:
+            print("Erro inserindo produto: %s" % e)
+        return redirect("/produto")
     return render(request, template_name='produto/produto-create.html',status=200)
 
 #cd loja

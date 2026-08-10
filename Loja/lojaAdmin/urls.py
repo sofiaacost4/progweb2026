8e3a466173d0faa9bdb05370fbaa94911503683b
sofiaacost4 <k.sofia@escolar.ifrn.edu.br>
2026-08-10 10:29:15 -0300

    cap 10 pt 1

--- Loja/lojaAdmin/urls.py
+++ Loja/lojaAdmin/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', include('loja.urls.HomeUrls')),
     path('produto/', include('loja.urls.ProdutoUrls')),
     path('usuario/', include('loja.urls.UsuarioUrls')),
+    path('', include('loja.urls.AuthUrls')),
     path('categoria/', include('loja.urls.CategoriaUrls')),
     path('fabricante/', include('loja.urls.FabricanteUrls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
